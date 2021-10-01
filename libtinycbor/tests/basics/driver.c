997c7df440673f716825d1a67553d073b1fa0229
Francois Kritzinger <francois@codesynthesis.com>
2021-10-01 10:06:19 +0200

    Add test for long conversion in format functions

--- libtinycbor/tests/basics/driver.c
+++ libtinycbor/tests/basics/driver.c
@@ -1,34 +1,53 @@
-#include <libtinycbor/cbor.h>
-
-// Encode/decode an integer value to/from a buffer.
+// Silence "sprintf is unsafe" warnings on Windows.
 //
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>              // sprintf
+#undef NDEBUG
+#include <assert.h>
+#include <inttypes.h>           // PRIu64
+
+#include <libtinycbor/cbor.h>
 
 int
 main ()
 {
-  uint8_t b[16]; // The buffer.
-  int v = 123;   // The integer value.
-
-  // Encode the value.
+  // Test that an integer can be successfully encoded to and decoded from a
+  // buffer.
   //
   {
-    CborEncoder e;
-    cbor_encoder_init (&e, b, sizeof (b), 0);
-    cbor_encode_int (&e, v);
+    uint8_t b[16]; // The buffer.
+    int v = 123;   // The integer value.
+
+    // Encode the value.
+    //
+    {
+      CborEncoder e;
+      cbor_encoder_init (&e, b, sizeof (b), 0);
+      cbor_encode_int (&e, v);
+    }
+
+    v = 0;
+
+    // Decode the value.
+    //
+    {
+      CborParser p;
+      CborValue cv;
+      cbor_parser_init (b, sizeof (b), 0, &p, &cv);
+      cbor_value_get_int (&cv, &v);
+    }
+
+    assert (v == 123);
   }
 
-  v = 0;
-
-  // Decode the value.
+  // Test that the printf-like functions correctly convert a long value to
+  // string (see README-DEV for details).
   //
   {
-    CborParser p;
-    CborValue cv;
-    cbor_parser_init (b, sizeof (b), 0, &p, &cv);
-    cbor_value_get_int (&cv, &v);
+    char s[32] = "";
+    sprintf (s, "%" PRIu64, 18446744073709551615ULL);
+    assert (strcmp (s, "18446744073709551615") == 0);
   }
 
-  // Fail if the encoded and decoded values are not the same.
-  //
-  return v == 123 ? 0 : 1;
+  return 0;
 }
